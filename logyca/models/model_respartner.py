6b4290e2b642c8d8f66836df6236c7c70b8c1d9c
lbuitron <57004177+lbuitron@users.noreply.github.com>
2020-02-11 14:00:29 -0500

    Se agrega la logica del metodo _update_fe_info_contact

--- logyca/models/model_respartner.py
+++ logyca/models/model_respartner.py
@@ -152,21 +152,22 @@ class ResPartner(models.Model):
     x_info_creation_history = fields.Char(string='Información de creación y modificación historica', track_visibility='onchange')
 
     @api.depends('name')
+    @api.model
     def _update_fe_info_contact(self):
-        # self.env.cr.execute("""Select a.email,a.name,a.phone From res_partner as a 
-                                # Inner Join logyca_contact_types_res_partner_rel as b on a.id = b.res_partner_id 
-                                # Inner join logyca_contact_types as fe on b.logyca_contact_types_id = fe.id and fe.code = 'FE' 
-                                # Inner Join res_partner as c on a.parent_id = c.id Where c.name='%s'""" % self.name)
-        # result = tuple()
-        # result = self.env.cr.dictfetchall()
+        self.env.cr.execute("""Select a.email,a.name,a.phone From res_partner as a 
+                                Inner Join logyca_contact_types_res_partner_rel as b on a.id = b.res_partner_id 
+                                Inner join logyca_contact_types as fe on b.logyca_contact_types_id = fe.id and fe.code = 'FE' 
+                                Inner Join res_partner as c on a.parent_id = c.id Where c.name='%s'""" % self.name)
+        result = tuple()
+        result = self.env.cr.dictfetchall()
         email = ""
         name = ""
         phone = ""
 
-        # for ids in result:
-            # email = ids.get('email')
-            # name = ids.get('name')
-            # phone = ids.get('phone')
+        for ids in result:
+            email = ids.get('email')
+            name = ids.get('name')
+            phone = ids.get('phone')
 
         self.x_email_contact_invoice_electronic = email
         self.x_name_contact_invoice_electronic = name
