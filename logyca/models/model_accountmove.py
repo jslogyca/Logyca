dc3599a62ad9f43eda1d0f5373bdc1a1c2c619fb
lbuitron <57004177+lbuitron@users.noreply.github.com>
2020-03-30 20:12:50 -0500

    Se comenta el metodo _inherit_fiscal_position
    Se comenta el metodo _inherit_fiscal_position para el modelo account_move.

--- logyca/models/model_accountmove.py
+++ logyca/models/model_accountmove.py
@@ -10,8 +10,7 @@ class AccountMove(models.Model):
     x_country_account_id = fields.Many2one('res.country', string='País', track_visibility='onchange')
     #NUMERO DE ORDEN DE COMPRA
     x_num_order_purchase = fields.Char(string='Número orden de compra', track_visibility='onchange')
-    fiscal_position_id = fields.Many2one('account.fiscal.position', string='Posición fiscal', track_visibility='onchange')
-
+    
     @api.onchange('partner_id')
     def _onchange_partner_id_country(self):
         
@@ -22,16 +21,16 @@ class AccountMove(models.Model):
             }
         self.update(values)
 
-    @api.onchange('invoice_origin')
-    def _inherit_fiscal_position(self):        
-        #sale_order = self.env['sale_order'].browse(self.invoice_origin.id)
-        for move in self:
-            self.env.cr.execute("Select B.fiscal_position_id From account_move a Inner join sale_order b on a.invoice_origin = b.name WHERE a.invoice_origin = '"+str(move.invoice_origin)+"'")
+    # @api.onchange('invoice_origin')
+    # def _inherit_fiscal_position(self):        
+    #     #sale_order = self.env['sale_order'].browse(self.invoice_origin.id)
+    #     for move in self:
+    #         self.env.cr.execute("Select B.fiscal_position_id From account_move a Inner join sale_order b on a.invoice_origin = b.name WHERE a.invoice_origin = '"+str(move.invoice_origin)+"'")
                 
-            fiscal_position_id = set(res[0] for res in self.env.cr.fetchall())
+    #         fiscal_position_id = set(res[0] for res in self.env.cr.fetchall())
 
-            values = {
-                    'fiscal_position_id': fiscal_position_id ,                
-                }
+    #         values = {
+    #                 'fiscal_position_id': fiscal_position_id ,                
+    #             }
 
-            self.update(values)
+    #         self.update(values)
