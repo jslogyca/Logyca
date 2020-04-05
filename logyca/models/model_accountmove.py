729b32795211aab3fd61adb1b7e0ac93a13c3164
lbuitron <57004177+lbuitron@users.noreply.github.com>
2020-04-05 13:12:52 -0500

    Se agregaron los campos de FE
    Se agregaron los campos de FE para el modelo de account.move.
    Campos:
    x_date_send_dian
    x_send_dian
    x_cufe_dian

--- logyca/models/model_accountmove.py
+++ logyca/models/model_accountmove.py
@@ -10,7 +10,12 @@ class AccountMove(models.Model):
     x_country_account_id = fields.Many2one('res.country', string='País', track_visibility='onchange')
     #NUMERO DE ORDEN DE COMPRA
     x_num_order_purchase = fields.Char(string='Número orden de compra', track_visibility='onchange')
+    #FACTURACIÓN ELECTRONICA
+    x_date_send_dian = fields.Datetime(string='Fecha de envío a la DIAN')
+    x_send_dian = fields.Boolean(string='Enviado a la DIAN')
+    x_cufe_dian = fields.Char(string='CUFE - Código único de facturación electrónica')
     
+
     @api.onchange('partner_id')
     def _onchange_partner_id_country(self):
         
