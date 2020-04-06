43db92285985e59e37542d0708c83ad4c46f8085
lbuitron <57004177+lbuitron@users.noreply.github.com>
2020-04-05 23:36:25 -0500

    Se agrega el campo x_history_partner
    Se agrega el campo x_history_partner al modelo res_partner.

--- logyca/models/model_respartner.py
+++ logyca/models/model_respartner.py
@@ -148,7 +148,8 @@ class ResPartner(models.Model):
 
     #CAMPOS HISTORICOS
     x_info_creation_history = fields.Char(string='Información de creación y modificación historica', track_visibility='onchange')
-    
+    x_history_partner = fields.Many2many('logyca.history_partner', string='Historial SalesForce')
+
     @api.depends('x_asset_range')
     def _date_update_asset(self):
         self.x_date_update_asset = fields.Date.today()
