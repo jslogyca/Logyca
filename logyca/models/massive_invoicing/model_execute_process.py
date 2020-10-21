32a6b4021ac9700da9127a60a4069e86e7a3c01f
Luis Buitrón <lbuitron@logyca.com>
2020-10-21 20:31:23 +0000

    Seventh develop of facturación masiva

--- logyca/models/massive_invoicing/model_execute_process.py
+++ logyca/models/massive_invoicing/model_execute_process.py
@@ -13,13 +13,11 @@ class x_MassiveInvoicingProcess(models.TransientModel):
     
     year = fields.Integer(string='Año proceso', required=True)
     invoicing_companies = fields.Many2one('massive.invoicing.companies', string='Empresas a ejecutar', required=True)
+    #Respuesta API
+    x_enpoint_code_assignment = fields.One2many('massive.invoicing.enpointcodeassignment', 'process_id', string = 'Respuesta API - Aginación de códigos', readonly=True)
     #Cantidad de prefijos
-    cant_prefixes_ds = fields.Integer(string='Cantidad de códigos 4D a 7D', readonly=True) 
-    cant_prefixes_mixed = fields.Integer(string='Cantidad de códigos mixtos', readonly=True) 
-    cant_prefixes_gtin = fields.Integer(string='Cantidad de códigos GTIN8', readonly=True) 
-    cant_prefixes_wrong = fields.Integer(string='Cantidad de códigos con inconsistencia (GLN, GL13, 8D)', readonly=True) 
-    
-    
+    x_partner_calculation_prefixes = fields.One2many('massive.invoicing.partnercalculationprefixes', 'process_id', string = 'Cálculo de cantidad de prefijos', readonly=True)
+        
     def name_get(self):
         result = []
         for record in self:            
@@ -57,33 +55,161 @@ class x_MassiveInvoicingProcess(models.TransientModel):
         url_api = self.invoicing_companies.url_enpoint_code_assignment
         response = requests.get(url_api,data=body_api, headers=headers_api)
         
+        #Eliminar llamado si ya existe
+        enpointcodeassignment_exists = self.env['massive.invoicing.enpointcodeassignment'].search([('process_id', '=', self.id)])
+        enpointcodeassignment_exists.unlink()
+        
+        for data in response.json():
+            partner_vat = data['Nit']
+            enpointcodeassignment_vals = data
+            enpointcodeassignment_vals['process_id'] = self.id
+            
+            for partner in self.invoicing_companies.thirdparties:
+                if partner.vat == partner_vat:
+                    partner_id = partner.id
+                    enpointcodeassignment_vals['partner_id'] = partner_id                    
+             
+            #raise ValidationError(_(enpointcodeassignment_vals)) 
+            enpointcodeassignment = self.env['massive.invoicing.enpointcodeassignment'].create(enpointcodeassignment_vals)
+            
         return response.json()
     
     #Cálculo de cantidad de prefijos
     def calculation_of_number_prefixes(self):  
-        invoicing_companies = self.enpoint_code_assignment()
         
-        prefixes_ds = []
-        cant_prefixes_ds = 0
-        prefixes_gtin = []
-        cant_prefixes_gtin = 0  
-        prefixes_wrong = []
-        cant_prefixes_wrong = 0
+        #Eliminar llamado si ya existe
+        partnercalculationprefixes_exists = self.env['massive.invoicing.partnercalculationprefixes'].search([('process_id', '=', self.id)])
+        partnercalculationprefixes_exists.unlink()
         
-        for partner in invoicing_companies:
-            partner_range = partner['Rango']
+        for partner in self.invoicing_companies.thirdparties:
+            prefixes_ds = []
+            cant_prefixes_ds = 0
+            prefixes_fixed_weight = []
+            cant_prefixes_fixed_weight = 0
+            prefixes_variable_weight = []
+            cant_prefixes_variable_weight = 0
+            prefixes_mixed = []
+            cant_prefixes_mixed = 0
+            prefixes_gtin = []
+            cant_prefixes_gtin = 0  
+            prefixes_gl = []
+            cant_prefixes_gl = 0
+            prefixes_delete_fixes = []
+            prefixes_delete_variable = []
+            enpointcodeassignment = self.env['massive.invoicing.enpointcodeassignment'].search([('process_id', '=', self.id),('partner_id','=',partner.id)])
             
-            if partner_range in ['4D','5D','6D','7D']:
-                prefixes_ds.append(partner)
-            if partner_range in ['GTIN8']:
-                prefixes_gtin.append(partner)
-            if partner_range in ['GLN','GL13','8D']:
-                prefixes_wrong.append(partner)
+            for data in enpointcodeassignment:            
+                partner_vat = data.Nit
+                partner_range = data.Rango
         
-        cant_prefixes_ds = len(prefixes_ds)
-        cant_prefixes_gtin = len(prefixes_gtin)
-        cant_prefixes_wrong = len(prefixes_wrong)
+                if partner_range in ['4D','5D','6D','7D','8D']:
+                    prefixes_ds.append(data.PrefixId)
+                if partner_range in ['PesoFijo']:
+                    prefixes_fixed_weight.append(data.PrefixId)
+                if partner_range in ['Peso variable']:
+                    prefixes_variable_weight.append(data.PrefixId)
+                if partner_range in ['GTIN8']:
+                    prefixes_gtin.append(data.PrefixId)
+                if partner_range in ['GLN','GL13']:
+                    prefixes_gl.append(data.PrefixId)
+            
+            #Calculo prefijos mixtos
+            for variable_weight in prefixes_variable_weight:
+                if variable_weight[:2] == '29':
+                    for fixed_weight in prefixes_fixed_weight:
+                        if fixed_weight[:3] == '770' or fixed_weight[:3] == '771':
+                            if variable_weight[-3:] == fixed_weight[-3:]:                                
+                                #Se agrega en prefijos mixtos
+                                prefixes_mixed.append(variable_weight+"|"+fixed_weight)
+                                #Se eliminan de peso variable y peso fijo
+                                prefixes_delete_fixes.append(fixed_weight)
+                                prefixes_delete_variable.append(variable_weight)
+            
+            #Eliminar prefijos segun calculo de mixtos
+            for delete_f in prefixes_delete_fixes:
+                prefixes_fixed_weight.remove(delete_f)
+            for delete_v in prefixes_delete_variable:
+                prefixes_variable_weight.remove(delete_v)
+            
+            #Conteo
+            cant_prefixes_ds = len(prefixes_ds)
+            cant_prefixes_fixed_weight = len(prefixes_fixed_weight)
+            cant_prefixes_variable_weight = len(prefixes_variable_weight)
+            cant_prefixes_mixed = len(prefixes_mixed)
+            cant_prefixes_gtin = len(prefixes_gtin)
+            cant_prefixes_gl = len(prefixes_gl)
+            
+            if cant_prefixes_ds+cant_prefixes_fixed_weight+cant_prefixes_variable_weight+cant_prefixes_mixed+cant_prefixes_gtin+cant_prefixes_gl > 0:
+                have_prefixes = '1'
+            else:
+                have_prefixes = '2'
+            
+            partnercalculationprefixes_values = {
+                'process_id': self.id,
+                'partner_id': partner.id,
+                'vat': partner.vat,
+                'have_prefixes': have_prefixes,
+                'cant_prefixes_ds': cant_prefixes_ds,
+                'prefixes_ds': str(prefixes_ds),
+                'cant_prefixes_fixed_weight': cant_prefixes_fixed_weight,
+                'prefixes_fixed_weight': prefixes_fixed_weight,
+                'cant_prefixes_variable_weight': cant_prefixes_variable_weight,
+                'prefixes_variable_weight': prefixes_variable_weight,
+                'cant_prefixes_mixed': cant_prefixes_mixed,
+                'prefixes_mixed': prefixes_mixed,                
+                'cant_prefixes_gtin': cant_prefixes_gtin,
+                'prefixes_gtin': prefixes_gtin,                
+                'cant_prefixes_gl': cant_prefixes_gl,
+                'prefixes_gl': prefixes_gl,
+            }
+            
+            partnercalculationprefixes = self.env['massive.invoicing.partnercalculationprefixes'].create(partnercalculationprefixes_values)
         
-        self.cant_prefixes_ds = cant_prefixes_ds
-        self.cant_prefixes_gtin = cant_prefixes_gtin
-        self.cant_prefixes_wrong = cant_prefixes_wrong
+        
+class x_MassiveInvoicingEnpointCodeAssignment(models.TransientModel):
+    _name = 'massive.invoicing.enpointcodeassignment'
+    _description = 'Massive Invoicing - Enpoint Code Assignment'
+    
+    process_id = fields.Many2one('massive.invoicing.process',string='Proceso FacMasiva', required=True, ondelete='cascade')
+    Nit = fields.Char(string='Nit', required=True)
+    RazonSocial = fields.Char(string='Razon Social', required=True)
+    partner_id = fields.Many2one('res.partner', string='Cliente', required=True)
+    IdEstado = fields.Integer(string='Id Estado', required=True)
+    EstadoPrefijo = fields.Char(string='Estado', required=True)
+    IdRango = fields.Integer(string='Id Rango', required=True)
+    Rango = fields.Char(string='Rango', required=True)
+    IdEsquema = fields.Integer(string='Id Esquema', required=True)
+    Esquema = fields.Char(string='Esquema', required=True)
+    CapacidadPrefijo = fields.Integer(string='Capacidad Prefijo', required=True)
+    PrefixId = fields.Char(string='Prefijo', required=True)
+    FechaAsignacion = fields.Date(string='Fecha Asignación', required=True)
+    
+class x_MassiveInvoicingPartnerCalculationPrefixes(models.TransientModel):
+    _name = 'massive.invoicing.partnercalculationprefixes'
+    _description = 'Massive Invoicing - Partner Calculation Prefixes'
+    
+    process_id = fields.Many2one('massive.invoicing.process',string='Proceso FacMasiva', required=True, ondelete='cascade')
+    partner_id = fields.Many2one('res.partner', string='Cliente', required=True)
+    vat = fields.Char(string='Nit', required=True)
+    have_prefixes = fields.Selection([
+                                        ('1', 'Si'),
+                                        ('2', 'No - REVISAR'),                                        
+                                    ], string='Tiene códigos', readonly=True)
+    
+    cant_prefixes_ds = fields.Integer(string='Cant. códigos 4D a 8D', readonly=True) 
+    prefixes_ds = fields.Char(string='Códigos 4D a 8D', readonly=True)
+    
+    cant_prefixes_fixed_weight = fields.Integer(string='Cant. códigos peso fijo', readonly=True) 
+    prefixes_fixed_weight = fields.Char(string='Códigos peso fijo', readonly=True)
+    
+    cant_prefixes_variable_weight = fields.Integer(string='Cant. códigos peso variable', readonly=True) 
+    prefixes_variable_weight = fields.Char(string='Códigos peso variable', readonly=True)
+    
+    cant_prefixes_mixed = fields.Integer(string='Cant. códigos mixtos', readonly=True) 
+    prefixes_mixed = fields.Char(string='Códigos mixtos', readonly=True)
+    
+    cant_prefixes_gtin = fields.Integer(string='Cant. códigos GTIN8', readonly=True) 
+    prefixes_gtin = fields.Char(string='Códigos GTIN8', readonly=True)
+    
+    cant_prefixes_gl = fields.Integer(string='Cant. códigos GLN & GL13', readonly=True) 
+    prefixes_gl = fields.Char(string='Códigos GLN & GL13', readonly=True)
