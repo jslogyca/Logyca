e39e3431da1b83bfb57626645a9993d47a659863
jsocampo <jsocampo@logyca.com>
2021-05-04 17:31:00 -0500

    [ADD] remove_duplicate_button

---
+++ remove_duplicate_button/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 LOGYCA SERVICIOS
+
+{
+    "name": "Remove Duplicate Button",
+    "summary": "This module helps you to hide the duplicate button for product and partner",
+    "version": "13.0.0.0.0",
+    "author": "LOGYCA SERVICIOS S.A.S, "
+    "JUAN SEBASTIÁN OCAMPO OSPINA"
+    "license": "AGPL-3",
+    "maintainer": "LOGYCA SERVICIOS S.A.S",
+    "category": "Extra Tools",
+    "depends": ["base"],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+}
