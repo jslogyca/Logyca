1a81fc979065d652da360d273760be4b23385d3c
jsocampo <jsocampo@logyca.com>
2021-06-23 17:12:17 -0500

    [ADD] logyca_mass_mailing_theme

---
+++ logyca_mass_mailing_theme/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Logyca Mass Mailing Theme',
+    'summary': 'Logyca Mass Mailing Theme',
+    'description': """
+
+    """,
+    'version': '13.0.1.0.0',
+    'sequence': 115,
+    'category': 'Marketing',
+    'depends': [
+        'mass_mailing',
+    ],
+    'data': [
+        'static/src/xml/assets.xml',
+        'views/mass_mailing_themes_templates.xml'
+    ],
+    'qweb': [],
+    'installable': True,
+    'auto_install': True,
+}
