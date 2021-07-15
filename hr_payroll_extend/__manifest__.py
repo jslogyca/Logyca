87fba51986d3f23dbda47048760f3c285ecd0015
Lorena Torres <lorenat0210@gmail.com>
2021-07-15 12:53:59 +0000

    Ajustes para nómina

---
+++ hr_payroll_extend/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Payroll Extend',
+    'summary': 'Description',
+    'version': '1.1',
+    'category': 'Accounting/Accounting',
+    'website': 'https://logyca.com',
+    'author': 'LOGYCA',
+    'license': '',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'hr',
+        'hr_payroll',
+        ],
+    'description': '''
+
+========================
+
+''',    
+    'data': [
+        'views/hr_employee_view.xml',
+    ],
+    'qweb': [
+    ]
+}
