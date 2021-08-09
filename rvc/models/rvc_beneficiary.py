d43b8df2dddf394f84e4fd23d3f94f1145919ea2
jsocampo <jsocampo@logyca.com>
2021-08-09 09:51:47 -0500

    [IMP] beneficiary and sponsor loads contact details, activate and deactivate buttons in beneficiary and sponsor

--- rvc/models/rvc_beneficiary.py
+++ rvc/models/rvc_beneficiary.py
@@ -7,7 +7,7 @@ from odoo.exceptions import ValidationError
 class RVCBeneficiary(models.Model):
     _name = 'rvc.beneficiary'
     _description = 'RVC Beneficiary'
-    # _inherit = ['mail.thread']
+    _inherit = ['mail.thread']
     _rec_name = 'name'
 
     name = fields.Char(string='Name', track_visibility='onchange')
@@ -37,6 +37,16 @@ class RVCBeneficiary(models.Model):
 
     def name_get(self):
         return [(benef.id, '%s - %s' % (benef.vat, benef.partner_id.name)) for benef in self]
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        #es un contacto
+        if self.partner_id.is_company == False or self.partner_id.parent_id:
+            partner = self.partner_id
+            self.contact_name = partner.name
+            self.contact_phone = partner.phone if partner.phone else partner.mobile
+            self.contact_email = partner.email
+            self.contact_position = partner.x_contact_job_title.name if partner.x_contact_job_title else ''
 
     @api.model
     def create(self, vals):
--- rvc/models/rvc_sponsored.py
+++ rvc/models/rvc_sponsored.py
@@ -12,7 +12,7 @@ class RVCSponsored(models.Model):
     name = fields.Char(string='Name')
     company_id = fields.Many2one('res.company', string='Company', required=True, default=lambda self: self.env.company)
     partner_id = fields.Many2one('res.partner', string='Patrocinador')
-    vat = fields.Char('NIT')
+    vat = fields.Char('NIT', related='partner_id.vat')
     phone = fields.Char('Phone', related='partner_id.phone')
     email = fields.Char('Email', related='partner_id.email')
     x_sector_id = fields.Many2one('logyca.sectors', string='Sector', related='partner_id.x_sector_id', readonly=True, store=True)
@@ -40,38 +40,42 @@ class RVCSponsored(models.Model):
 
     def name_get(self):
         return [(sponsored.id, '%s - %s' % (sponsored.partner_id.name, sponsored.vat)) for sponsored in self]
-
-
-    @api.onchange('vat')
-    def _onchange_vat(self):
-        if self.vat:
-            partner_id = self.env['res.partner'].search([('vat','=',self.vat), ('is_company','=',True)])
-            if partner_id:
-                self.write({'partner_id': partner_id.id})
-            else:
-                raise ValidationError('La empresa no esta registrada en Odoo')
-
-
+    
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        #es un contacto
+        if self.partner_id.is_company == False or self.partner_id.parent_id:
+            partner = self.partner_id
+            self.contact_name = partner.name
+            self.contact_phone = partner.phone if partner.phone else partner.mobile
+            self.contact_email = partner.email
+            self.contact_position = partner.x_contact_job_title.name if partner.x_contact_job_title else ''
+    
     @api.model
     def create(self, vals):
-        if vals.get('partner_id', False):
-            partner_id = self.env['res.partner'].search([('id','=',vals.get('partner_id', False))])
-            if partner_id and not partner_id.active:
-                raise ValidationError('La empresa esta activa')
-            if partner_id and not partner_id.x_type_vinculation.code in ('01'):
-                raise ValidationError('La empresa No es Miembro')
+        partner_id = self.env['res.partner'].search([('id','=',vals.get('partner_id', False))])
+        if partner_id:
+            if partner_id.active == False:
+                raise ValidationError('¡Error de validación! La empresa NO está activa.')
+            if partner_id.x_type_vinculation and partner_id.x_type_vinculation.code == '01':
+                raise ValidationError('¡Error de validación! La empresa es miembro.')
         return super(RVCSponsored, self).create(vals)
 
 
     def write(self, vals):
-        if vals.get('partner_id', False):
-            partner_id = self.env['res.partner'].search([('id','=',vals.get('partner_id', False))])
-            if partner_id and not partner_id.active:
-                raise ValidationError('La empresa esta activa')
-            if partner_id and not partner_id.x_type_vinculation.code in ('01'):
-                raise ValidationError('La empresa No es Miembro')
+        partner_id = self.env['res.partner'].search([('id','=',vals.get('partner_id', False))])
+        if partner_id:
+            if partner_id.active == False:
+                raise ValidationError('¡Error de validación! La empresa NO está activa.')
+            if partner_id.x_type_vinculation and partner_id.x_type_vinculation.code == '01':
+                raise ValidationError('¡Error de validación! La empresa es miembro.')
         return super(RVCSponsored, self).write(vals)
 
                 
+    def deactivate_beneficiary(self):
+        for rec in self:
+            rec.active = False
 
-#
+    def activate_beneficiary(self):
+        for rec in self:
+            rec.active = True
