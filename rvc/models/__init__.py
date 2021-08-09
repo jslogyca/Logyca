e943b77f76151525f4c1644864e916393cabcdd1
jsocampo <jsocampo@logyca.com>
2021-08-09 16:13:52 -0500

    [FIX] bugfix d788d...caa0 [IMP] beneficiary and sponsor onchange fill contact details

--- rvc/models/__init__.py
+++ rvc/models/__init__.py
@@ -5,5 +5,4 @@ from . import rvc_sponsored
 from . import config_rvc
 from . import log_import_rvc
 from . import product_rvc
-from . import agreement_rvc
 from . import benefits_admon
--- rvc/models/rvc_sponsored.py
+++ rvc/models/rvc_sponsored.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, tools, _
+from odoo import _, api, fields, models, tools
 from odoo.exceptions import ValidationError
 
 
@@ -37,20 +37,28 @@ class RVCSponsored(models.Model):
         ('partner_id_company_uniq', 'unique (vat,company_id)', 'La empresa halonadora ya esta creada')
     ]    
 
-
     def name_get(self):
         return [(sponsored.id, '%s - %s' % (sponsored.partner_id.name, sponsored.vat)) for sponsored in self]
-    
+
     @api.onchange('partner_id')
     def onchange_partner_id(self):
         #es un contacto
         if self.partner_id.is_company == False or self.partner_id.parent_id:
+            self.clear_contact_fields()
+
             partner = self.partner_id
+
             self.contact_name = partner.name
             self.contact_phone = partner.phone if partner.phone else partner.mobile
             self.contact_email = partner.email
             self.contact_position = partner.x_contact_job_title.name if partner.x_contact_job_title else ''
     
+    def clear_contact_fields(self):
+        self.contact_name = ""
+        self.contact_phone = ""
+        self.contact_email = ""
+        self.contact_position = ""
+
     @api.model
     def create(self, vals):
         partner_id = self.env['res.partner'].search([('id','=',vals.get('partner_id', False))])
