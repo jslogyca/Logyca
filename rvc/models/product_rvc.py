0760b29ed417ac6487c6b9196674957cf8a478ab
jsocampo <jsocampo@logyca.com>
2021-09-20 17:37:53 -0500

    [IMP] replacing sub_product_ids for colabora_level

--- rvc/models/product_rvc.py
+++ rvc/models/product_rvc.py
@@ -16,22 +16,3 @@ class ProductRVC(models.Model):
     company_id = fields.Many2one('res.company', string='Company', required=True, default=lambda self: self.env.company)
     state = fields.Selection([('activo', 'Activo'), 
                             ('inactivo', 'Inactivo')], string="State", default='activo')
-    sub_product_ids = fields.One2many('sub.product.rvc', 'product_id', string='Sub-Productos')
-    
-
-
-
-class SubProductRVC(models.Model):
-    _name = 'sub.product.rvc'
-    _description = 'Product RVC'
-    _rec_name = 'name'
-
-
-    name = fields.Char(string='Product')
-    code = fields.Char(string='Code')
-    state = fields.Selection([('activo', 'Activo'), 
-                            ('inactivo', 'Inactivo')], string="State", default='activo')
-    product_id = fields.Many2one('product.rvc', string='Product')
-    min_qty = fields.Integer('Min.')
-    max_qty = fields.Integer('Max.')
-                            
