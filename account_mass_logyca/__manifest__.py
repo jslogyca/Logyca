4c73cc2b80ff8cbafa392f0245164dbb6e7faf4b
Lorena Torres <lorenat0210@gmail.com>
2021-10-06 03:37:37 +0000

    Massive

---
+++ account_mass_logyca/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account Mass Logyca',
+    'summary': 'Description',
+    'version': '1.1',
+    'category': 'Accounting/Accounting',
+    'website': 'https://logyca.com',
+    'author': 'LOGYCA',
+    'license': '',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'account',
+        'logyca',
+        'base',
+        'account_move_extended',
+        ],
+    'description': '''
+
+========================
+
+''',    
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/config_discount_logycaedx_view.xml',
+        'views/res_partner_view.xml',
+        'views/partner_logycaedx_view.xml',
+        'data/config_discount_logycaedx_data.xml',
+    ],
+    'qweb': [
+    ]
+}
---
+++ account_mass_logyca/models/partner_logycaedx.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+from odoo import models, fields, api, _
+from odoo.exceptions import AccessError, ValidationError, UserError
+
+class PartnerLogycaEDX(models.Model):
+    _name= 'partner.logycaedx'
+    _description = "Partner Logyca EDX"
+
+    config_discount_id = fields.Many2one('config.discount.logycaedx', string='Discount', tracking=True)
+    partner_id = fields.Many2one('res.partner', string='Partner', tracking=True)
+
+    def name_get(self):
+        return [(logycaedx.id, '%s - %s' % (logycaedx.config_discount_id.name, logycaedx.partner_id.name)) for logycaedx in self]
+
+    @api.model
+    def create(self, values):
+        if values.get('partner_id', False):
+            partner_id = self.env['res.partner'].search([('id', '=', values.get('partner_id', False))])
+            if partner_id:
+                partner_id.write({'logycax_discount_id': values.get('config_discount_id', False)})
+        return super(PartnerLogycaEDX, self).create(values)        
+
+    def write(self, values):
+        if values.get('partner_id', False):
+            partner_id = self.env['res.partner'].search([('id', '=', values.get('partner_id', False))])
+            if partner_id:
+                partner_id.write({'logycax_discount_id': self.config_discount_id.id})
+                self.partner_id.write({'logycax_discount_id': None})
+        return super(PartnerLogycaEDX, self).write(values)
