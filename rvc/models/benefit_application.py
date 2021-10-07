4e9ec6ca4bb60f2d72491457aabfe5b65c78593f
jsocampo <jsocampo@logyca.com>
2021-10-07 15:45:32 -0500

    [ADD] help in field origin of benefit.application

--- rvc/models/benefit_application.py
+++ rvc/models/benefit_application.py
@@ -60,7 +60,12 @@ class BenefitApplication(models.Model):
     benefit_name = fields.Selection(string="Nombre Beneficio", related='product_id.benefit_type', store=True, help="Technical field used for easy quering")
     origin = fields.Selection([('odoo', 'Odoo'), 
                                     ('tienda', 'Tienda Virtual'),
-                                    ('chatbot', 'ChatBot RVC')], string="Origen", track_visibility='onchange', default='odoo', readonly=True)
+                                    ('chatbot', 'ChatBot RVC')],
+                                    string="Origen",
+                                    track_visibility='onchange',
+                                    default='odoo',
+                                    readonly=True,
+                                    help="Este campo permite diferenciar las postulaciones RVC que provienen de Odoo, Tienda Virtual y ChatBot.")
 
     _sql_constraints = [
         ('benefits_partner_product_uniq', 'unique (partner_id, product_id)', '¡Error Guardando! La empresa seleccionada ya está aplicando para este beneficio.')
