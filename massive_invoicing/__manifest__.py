3549de1804f72f8e33362eb2af193198746dff94
Lorena Torres <lorenat0210@gmail.com>
2021-12-20 14:00:54 +0000

    Facturación Masiva 2022

---
+++ massive_invoicing/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Massive Invoicing Logyca',
+    'summary': 'Description',
+    'version': '1.1',
+    'category': 'Accounting/Accounting',
+    'website': 'https://logyca.com',
+    'author': 'LOGYCA',
+    'license': '',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'account',
+        'account_move_extended',
+        ],
+    'description': '''
+
+========================
+
+''',    
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/massive_income_tariff_view.xml',
+        'views/res_partner_view.xml',
+    ],
+    'qweb': [
+    ]
+}
