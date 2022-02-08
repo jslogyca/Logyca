e59a2103b4e992395c60b89e2ded4886635b09c5
Lorena Torres <lorenat0210@gmail.com>
2022-02-08 20:21:16 +0000

    Contabilización de la Nómina LOGYCA

---
+++ hr_payroll_account_extend/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Payroll Account',
+    'summary': 'Payroll Account',
+    'version': '1.1',
+    'category': 'Human Resources/Employees',
+    'website': 'https://logyca.com',
+    'author': 'LOGYCA / SERVICIOS SAS',
+    'license': '',
+    'support': 'lctorres@logyca.com',
+    'maintainer': 'LOGYCA / SERVICIOS SAS',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'base',
+        'hr',
+        'hr_payroll',
+    ],
+    'description': '''
+
+========================
+
+''',    
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_payroll_account_config_view.xml',
+        'views/hr_payslip_run_view.xml',
+        'views/hr_payroll_account_ss_view.xml',
+        'views/hr_employee.xml',
+    ],
+    'qweb': [
+    ]
+}
---
+++ hr_payroll_account_extend/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import hr_payroll_account_config
+from . import hr_payroll_account_ss
+from . import hr_payroll_account_line
+from . import hr_payslip_run
+from . import hr_employee
