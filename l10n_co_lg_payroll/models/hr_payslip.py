cbeb1a57c6b127d21bbff4854ab1cb34fc03cafb
Lorena Torres <lorenat0210@gmail.com>
2022-06-08 03:55:34 +0000

    Calculo PRIMA

--- l10n_co_lg_payroll/models/hr_payslip.py
+++ l10n_co_lg_payroll/models/hr_payslip.py
@@ -2,7 +2,20 @@
 
 
 from odoo import api, fields, models, tools, _
+from datetime import date, datetime, time, timedelta
+from dateutil.relativedelta import relativedelta
 
+
+def days_between(start_date, end_date):
+    #Add 1 day to end date to solve different last days of month 
+    #s1, e1 =  datetime.strptime(start_date,"%Y-%m-%d") , datetime.strptime(end_date,"%Y-%m-%d")  + timedelta(days=1)
+    s1, e1 =  start_date , end_date + timedelta(days=1)
+    #Convert to 360 days
+    s360 = (s1.year * 12 + s1.month) * 30 + s1.day
+    e360 = (e1.year * 12 + e1.month) * 30 + e1.day
+    #Count days between the two 360 dates and return tuple (months, days)
+    res = divmod(e360 - s360, 30)
+    return ((res[0] * 30) + res[1]) or 0
 
 class HrPayslip(models.Model):
     _inherit = 'hr.payslip'
@@ -26,6 +39,33 @@ class HrPayslip(models.Model):
             self.move_id.button_draft()
             # self.move_id.unlink()
         return self.write({"state": "cancel"})
+
+    def get_change_salary(self, employee_id, from_date, to_date):
+        self.env.cr.execute(
+            """
+                SELECT l.total
+                FROM hr_payslip_line l
+                INNER JOIN hr_payslip n on n.id=l.slip_id
+                INNER JOIN hr_employee e on e.id=n.employee_id
+                INNER JOIN hr_salary_rule r on r.id=l.salary_rule_id
+                WHERE r.base_prima = 'base_salary'
+                AND n.employee_id=%s and n.state='done'
+                and n.date_from BETWEEN %s and %s 
+                order by n.date_from desc """, (employee_id.id, from_date, to_date),
+        )
+        salary_ids = self.env.cr.fetchone()
+        if salary_ids:
+            month = 0
+            salary = 0
+            for salary_id in salary_ids:
+                if month <= 3:
+                    if salary == 0 or salary == salary_id:
+                        salary = salary_id
+                        prom_salary = False
+                        continue
+                    else:
+                        prom_salary = True
+        return prom_salary
 
     @api.model
     def _get_new_input_line_ids(self):
@@ -61,17 +101,31 @@ class HrPayslip(models.Model):
 
     def _get_lines_base_salayr_prima(self, from_date, to_date):
         self.ensure_one()
-        self.env.cr.execute(
-            """
-                SELECT coalesce(SUM(l.total),0.0)
-                FROM hr_payslip_line l
-                INNER JOIN hr_payslip n on n.id=l.slip_id
-                INNER JOIN hr_employee e on e.id=n.employee_id
-                INNER JOIN hr_salary_rule r on r.id=l.salary_rule_id
-                WHERE r.base_prima = 'base_salary'
-                AND n.employee_id=%s and n.state='done'
-                and n.date_from BETWEEN %s and %s """, (self.employee_id.id, from_date, to_date),
-        )
+        if self.get_change_salary(self.employee_id, from_date, to_date):
+            self.env.cr.execute(
+                """
+                    SELECT coalesce(SUM(l.total),0.0)
+                    FROM hr_payslip_line l
+                    INNER JOIN hr_payslip n on n.id=l.slip_id
+                    INNER JOIN hr_employee e on e.id=n.employee_id
+                    INNER JOIN hr_salary_rule r on r.id=l.salary_rule_id
+                    WHERE r.base_prima = 'base_salary'
+                    AND n.employee_id=%s and n.state='done'
+                    and n.date_from BETWEEN %s and %s """, (self.employee_id.id, from_date, to_date),
+            )
+        else:
+            self.env.cr.execute(
+                """
+                    SELECT c.wage*5
+                    FROM hr_payslip_line l
+                    INNER JOIN hr_payslip n on n.id=l.slip_id
+                    INNER JOIN hr_employee e on e.id=n.employee_id
+                    INNER JOIN hr_contract c on c.id=n.contract_id
+                    INNER JOIN hr_salary_rule r on r.id=l.salary_rule_id
+                    WHERE r.base_prima = 'base_salary'
+                    AND n.employee_id=%s and n.state='done'
+                    and n.date_from BETWEEN %s and %s limit 1 """, (self.employee_id.id, from_date, to_date),
+            )            
         res = self.env.cr.fetchone()
         return res and res[0] or 0.0
 
@@ -111,6 +165,10 @@ class HrPayslip(models.Model):
         self.ensure_one()
         if to_date is None:
             to_date = fields.Date.today()
+        days_period = 0.0
+        day_to = datetime.strptime(from_date,"%Y-%m-%d")
+        if str(self.contract_id.date_start) > from_date:
+            days_period = (days_between(day_to, self.contract_id.date_start))-1
         self.env.cr.execute(
             """
                 SELECT coalesce(sum(number_of_days),'0.0')
@@ -120,7 +178,7 @@ class HrPayslip(models.Model):
                 AND leave_prima is True""", (self.employee_id.id, from_date, to_date),
         )
         res = self.env.cr.fetchone()
-        return res and res[0] or 0.0
+        return res and res[0]+days_period or 0.0
 
 def average_salary_prima(payslip, categories, worked_days, inputs, from_date, to_date):
     base_prima = payslip.dict._get_lines_base_salayr_prima(from_date, to_date)
