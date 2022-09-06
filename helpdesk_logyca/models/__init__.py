9b3303ad6d6d25cfdfa7a39e0d356d415f38f454
Lorena Torres <lorenat0210@gmail.com>
2022-09-06 03:09:13 +0000

    HelpDesk Support

--- helpdesk_logyca/models/__init__.py
+++ helpdesk_logyca/models/__init__.py
@@ -2,6 +2,8 @@
 
 from . import helpdesk_team
 from . import helpdesk_station
+from . import helpdesk_platform
 from . import helpdesk_service
 from . import helpdesk_ticket_subtype
 from . import helpdesk_ticket
+from . import helpdesk_ticket_type
