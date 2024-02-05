4023ac6700c733f756d4d325cb7338e3ea145802
Lorena Torres <lorenat0210@gmail.com>
2024-02-05 21:53:39 +0000

    [UPD - Marketplace]

---
+++ logyca_marketplace/models/rel_res_partner_marketplace.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import ValidationError, UserError
+from dateutil.relativedelta import relativedelta
+from datetime import datetime, date, timedelta
+from odoo.modules.module import get_module_resource
+
+
+class RelResPartnerMarketplace(models.Model):
+    _name = 'rel.res.partner.marketplace'
+    _description = 'Marketplace - Partner'
+
+    partner_id = fields.Many2one('res.partner', string='Seller', tracking=True, ondelete='restrict')
+    marketplace_id = fields.Many2one('res.partner.marketplace', string="Marketplace", copy=False,)
+    name = fields.Char(string='Name', tracking=True)
+
+    def name_get(self):
+        return [(marketplace.id, '%s' % (marketplace.marketplace_id.partner_id.name)) for marketplace in self]
---
+++ logyca_marketplace/models/res_partner.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, api, fields, models, tools
+from odoo.exceptions import ValidationError
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    marketplace_ids = fields.One2many('rel.res.partner.marketplace', "partner_id", string="Marketplace", copy=False,)
