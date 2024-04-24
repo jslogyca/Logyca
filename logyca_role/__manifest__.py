8350d9dace4b5d10d4b1eb29de54af037cd72796
Lorena Torres <lorenat0210@gmail.com>
2024-04-24 03:11:50 +0000

    [UPD - Roles]

---
+++ logyca_role/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'LOGYCA Roles',
+    'summary': 'Description',
+    'version': '1.1',
+    'category': 'Accounting/Accounting',
+    'website': 'https://logyca.com',
+    'author': 'LOGYCA',
+    'license': '',
+    'application': True,
+    'installable': True,
+    'depends': [
+        'base',
+        ],
+    'description': '''
+
+========================
+
+''',    
+    'data': [
+        'security/security.xml',
+        'views/res_partner_view.xml',
+    ],
+    'qweb': [
+    ]
+}
