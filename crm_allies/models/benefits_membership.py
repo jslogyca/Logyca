d71e0811502bc6e6e6d123410e995216842155a7
Lorena Torres <lorenat0210@gmail.com>
2024-05-03 16:31:37 +0000

    Benefits Members

---
+++ crm_allies/models/benefits_membership.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError, UserError
+
+
+class BenefitsMembership(models.Model):
+    _name = 'benefits.membership'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'Benefits Membership'
+
+    name = fields.Char('Name')
+    code = fields.Char('Code')
+    active = fields.Boolean('Active', default=True)
+    company_id = fields.Many2one('res.company', string='Company', required=True,
+                                 default=lambda self: self.env.company)
+    categ_id = fields.Many2one('categ.benefits.membership', string="Type", required=True)    
