5285095093d3131f7621b7b25de0835ecac1d79a
Lorena Torres <lorenat0210@gmail.com>
2024-09-19 09:33:28 -0500

    Inputs en Nómina

---
+++ l10n_co_lg_payroll/wizard/hr_payroll_payslips_by_employees.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from collections import defaultdict
+from datetime import datetime, date, time
+import pytz
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class HrPayslipEmployees(models.TransientModel):
+    _inherit = 'hr.payslip.employees'
+
+    def compute_sheet(self):
+        self.ensure_one()
+        if not self.env.context.get('active_id'):
+            from_date = fields.Date.to_date(self.env.context.get('default_date_start'))
+            end_date = fields.Date.to_date(self.env.context.get('default_date_end'))
+            payslip_run = self.env['hr.payslip.run'].create({
+                'name': from_date.strftime('%B %Y'),
+                'date_start': from_date,
+                'date_end': end_date,
+            })
+        else:
+            payslip_run = self.env['hr.payslip.run'].browse(self.env.context.get('active_id'))
+
+        employees = self.with_context(active_test=False).employee_ids
+        if not employees:
+            raise UserError(_("You must select employee(s) to generate payslip(s)."))
+
+        payslips = self.env['hr.payslip']
+        Payslip = self.env['hr.payslip']
+
+        contracts = employees._get_contracts(payslip_run.date_start, payslip_run.date_end, states=['open', 'close'])
+        contracts._generate_work_entries(payslip_run.date_start, payslip_run.date_end)
+        work_entries = self.env['hr.work.entry'].search([
+            ('date_start', '<=', payslip_run.date_end),
+            ('date_stop', '>=', payslip_run.date_start),
+            ('employee_id', 'in', employees.ids),
+        ])
+        self._check_undefined_slots(work_entries, payslip_run)
+
+        validated = work_entries.action_validate()
+        if not validated:
+            work_entries_by_contract = defaultdict(lambda: self.env['hr.work.entry'])
+            for work_entry in work_entries.filtered(lambda w: w.state == 'conflict'):
+                work_entries_by_contract[work_entry.contract_id] |= work_entry
+            
+            for contract, work_entries in work_entries_by_contract.items():
+                conflicts = work_entries._to_intervals()
+                time_intervals_str = "\n - ".join(['', *["%s -> %s" % (s[0], s[1]) for s in conflicts._items]])
+            raise UserError(_("Some work entries of employee %s could not be validated. Time intervals to look for:%s") % (contract.employee_id.name, time_intervals_str))
+
+        default_values = Payslip.default_get(Payslip.fields_get())
+        for contract in contracts:
+            values = dict(default_values, **{
+                'employee_id': contract.employee_id.id,
+                'credit_note': payslip_run.credit_note,
+                'payslip_run_id': payslip_run.id,
+                'date_from': payslip_run.date_start,
+                'date_to': payslip_run.date_end,
+                'contract_id': contract.id,
+                'struct_id': self.structure_id.id or contract.structure_type_id.default_struct_id.id,
+            })
+            payslip = self.env['hr.payslip'].new(values)
+            payslip._onchange_employee()
+            values = payslip._convert_to_write(payslip._cache)
+            payslips += Payslip.create(values)
+        for payslip_input in payslips:
+            payslip_input.update_input_employee()
+        payslips.compute_sheet()
+        payslip_run.state = 'verify'
+
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'hr.payslip.run',
+            'views': [[False, 'form']],
+            'res_id': payslip_run.id,
+        }
