15b4d2eccf395ff4e0af84cf0f44f44b1994cef4
Lorena Torres <lorenat0210@gmail.com>
2024-10-21 04:59:10 -0500

    Accounting

--- hr_payroll_lg_account/models/hr_payslip.py
+++ hr_payroll_lg_account/models/hr_payslip.py
@@ -110,7 +110,7 @@ class HrPayslip(models.Model):
                     credit_sum += credit_line[2]["credit"] - credit_line[2]["debit"]
 
             if currency.compare_amounts(credit_sum, debit_sum) == -1:
-                acc_id = slip.journal_id.default_credit_account_id.id
+                acc_id = slip.journal_id.default_account_id.id
                 if not acc_id:
                     raise UserError(
                         _(
@@ -135,7 +135,7 @@ class HrPayslip(models.Model):
                 line_ids.append(adjust_credit)
 
             elif currency.compare_amounts(debit_sum, credit_sum) == -1:
-                acc_id = slip.journal_id.default_debit_account_id.id
+                acc_id = slip.journal_id.default_account_id.id
                 if not acc_id:
                     raise UserError(
                         _(
