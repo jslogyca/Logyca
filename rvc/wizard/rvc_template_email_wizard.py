64fd5b7f0770ccb5f6ae33880570a1a4334b8db1
jslogyca <jsocampo@logyca.com>
2024-11-26 16:56:13 -0500

    [FIX] RVC: broken reference to rvc_activations()

--- rvc/wizard/rvc_template_email_wizard.py
+++ rvc/wizard/rvc_template_email_wizard.py
@@ -3,7 +3,7 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, UserError
 from datetime import datetime
-from rvc.models import rvc_activations
+from models import rvc_activations
 import re
 import logging
 
