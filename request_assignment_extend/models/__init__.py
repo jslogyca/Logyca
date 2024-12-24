08ff4fc969c1f634b98feb7982fe5008ccb09990
Lorena Torres <lorenat0210@gmail.com>
2024-12-24 08:18:32 -0500

    Trazabilidad 2

---
+++ request_assignment_extend/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# from . import request_assignment_attach
+from . import request_partner_assignment_type
+from . import request_partner_code_assignment
+from . import res_partner
