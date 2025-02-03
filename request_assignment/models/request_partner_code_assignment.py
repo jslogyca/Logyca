9c4e2a1564b8ff1175f941f4164d8ab00c9ce3fa
jslogyca <jsocampo@logyca.com>
2025-02-03 10:59:24 -0500

    [IMP] request_assignment and request_assignment_extend adjustments as follows:
    - Order records in descending order based on the field `date_requisition`.
    - Change data types of the dates (`date_requisition` and `date_approved`) to include time.
    - Add field `state_requisition` to know the status of the request.
    - Add field `partner_receiver` to know which company receives the assignment.
    - Add new fields to the views but leave them commented out.
    - Add search view for the fields `id_requisition` and grouping for `type_requisition`.

--- request_assignment/models/request_partner_code_assignment.py
+++ request_assignment/models/request_partner_code_assignment.py
@@ -7,19 +7,20 @@ from datetime import date, timedelta
 class RequestPartnerCodeAssignment(models.Model):
     _name = 'request.partner.code.assignment'
     _description = 'Request Partner Code Assignment'
-    
+    _order_by = 'date_requisition desc'
+
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     vat = fields.Char(string='NIT')
     id_requisition = fields.Char(string='ID Requisition')
     type_requisition = fields.Many2one('request.partner.assignment.type', string='Request Type')
     user_requisition = fields.Char(string='User Requisition')
-    date_requisition = fields.Date(string='Date Requisition', default=fields.Date.context_today)
+    date_requisition = fields.Datetime(string='Date Requisition', default=fields.Datetime.now)
     user_approved = fields.Char(string='User Requisition Approved')
-    date_approved = fields.Date(string='Date Requisition Approved', default=fields.Date.context_today)
+    date_approved = fields.Datetime(string='Date Requisition Approved')
     comments_requisition = fields.Text(string='Comments Requisition')
     file_requisition = fields.Many2one('ir.attachment',string='Attachment')
+    state_requisition = fields.Char(string="State Requisition", help='Status of the requisition',)
 
     def name_get(self):
         return [(request.id, '%s - %s' %
                  (request.partner_id.name, request.type_requisition.name)) for request in self]
-
