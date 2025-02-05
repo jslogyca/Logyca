9141cd5351fd7b6f4bf0a4321a361c73eb5bc3e8
jsocampo <jsocampo@logyca.com>
2025-02-05 06:28:50 -0500

    [FIX] request_assignment: ui layout adjustments try 4

--- request_assignment_extend/models/request_assignment_attach.py
+++ request_assignment_extend/models/request_assignment_attach.py
@@ -6,7 +6,18 @@ from odoo import fields, models
 class RequestAssignmentAttachMent(models.Model):
     _name = 'request.assignment.attachment'
     _description = 'Request Assignment AttachMent'
-    
+
     name = fields.Char(string='Name')
     path = fields.Char(string='Path')
-    request_assignment_id = fields.Many2one('request.partner.code.assignment', string='Request Assignment')
+    request_assignment_id = fields.Many2one(
+        'request.partner.code.assignment',
+        string='Request Assignment'
+    )
+
+    def action_download_file(self):
+        """ This function returns an action that display a download button to the user"""
+        return {
+            'type': 'ir.actions.act_url',
+            'url': self.path,
+            'target': 'new',
+        }
