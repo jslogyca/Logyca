63e9dd3ffd18f96152d3edc7eb78cf31546f559e
jslogyca <jsocampo@logyca.com>
2025-02-11 05:38:36 -0500

    [FIX] request_assignment: debugging inverse relationship between res.partner and request.partner.code.assignment

--- request_assignment/models/res_partner.py
+++ request_assignment/models/res_partner.py
@@ -12,4 +12,4 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     request_assignment_ids = fields.One2many('request.partner.code.assignment', 'partner_id', string='Request Assignment')
-    request_received_ids = fields.One2many('request.partner.code.assignment', 'partner_receiver', string='Request Partner Receiver')
+    #request_received_ids = fields.One2many('request.partner.code.assignment', 'partner_receiver', string='Request Partner Receiver')
