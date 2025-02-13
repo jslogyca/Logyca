00902eddb0555bde93bd07637189fdcfdce63a87
jsocampo <jsocampo@logyca.com>
2025-02-13 05:52:30 -0500

    [ADD] request_assignment: added field request_received_ids as a one2many for inverse relationship

--- request_assignment/models/res_partner.py
+++ request_assignment/models/res_partner.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
-
 from datetime import date
 from dateutil.relativedelta import relativedelta
 from odoo.tools.misc import formatLang
 from odoo import api, fields, models, _
-
 from odoo.exceptions import ValidationError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    request_assignment_ids = fields.One2many('request.partner.code.assignment', 'partner_id', string='Request Assignment')
-    # request_received_ids = fields.One2many('request.partner.code.assignment', 'partner_receiver', string='Request Partner Receiver')
+    request_assignment_ids = fields.One2many(
+        'request.partner.code.assignment', 
+        'partner_id', 
+        string='Request Assignment'
+    )
+    request_received_ids = fields.One2many(
+        'request.partner.code.assignment', 
+        'partner_receiver_id', 
+        string='Request Partner Receiver'
+    )
