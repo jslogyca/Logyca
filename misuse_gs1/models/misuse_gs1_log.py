f9c7aa1c39d3fdc97a8545a72050c44b40006367
jslogyca <jsocampo@logyca.com>
2025-03-21 13:07:30 -0500

    [FIX] misuse_gs1: changes try 2

--- misuse_gs1/models/misuse_gs1_log.py
+++ misuse_gs1/models/misuse_gs1_log.py
@@ -1,5 +1,13 @@
 from odoo import models, fields, api
 
+class MisuseGS1LogFollowUp(models.Model):
+    _name = 'misuse.gs1.log.followup'
+    _description = 'Follow-up for Misuse GS1 Log'
+
+    log_id = fields.Many2one('misuse.gs1.log', string='Log', required=True, ondelete='cascade')
+    date_time = fields.Datetime(string='Date and Time', required=True, default=fields.Datetime.now)
+    comments = fields.Text(string='Comments', required=True)
+    gs1_agent_email = fields.Char(string='GS1 Agent Email', required=True)
 
 class MisuseGs1Log(models.Model):
     _name = 'misuse.gs1.log'
@@ -14,6 +22,7 @@ class MisuseGs1Log(models.Model):
     date_start = fields.Date(string='Start date', required=True)
     date_end = fields.Date(string='End date', required=True)
     remaining_time = fields.Integer(string='Remaining Time (days)')
+    follow_up = fields.One2many('misuse.gs1.log.followup', 'log_id', string='Follow-up')
 
 class MisuseGs1LogAttachment(models.Model):
     _name = 'misuse.gs1.log.attachment'
