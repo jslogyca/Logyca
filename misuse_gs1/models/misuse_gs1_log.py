19526ae982fc3082b721dc99629d6b00b7af1c48
jslogyca <jsocampo@logyca.com>
2025-03-21 15:15:06 -0500

    [FIX] misuse_gs1: changes try 5

--- misuse_gs1/models/misuse_gs1_log.py
+++ misuse_gs1/models/misuse_gs1_log.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class MisuseGs1LogAttachment(models.Model):
     _name = 'misuse.gs1.log.attachment'
@@ -12,7 +12,7 @@ class MisuseGS1LogFollowUp(models.Model):
     _name = 'misuse.gs1.log.followup'
     _description = 'Follow-up for Misuse GS1 Log'
 
-    log_id = fields.Many2one('misuse.gs1.log', string='Log', required=True, ondelete='cascade')
+    misuse_log_followup_id = fields.Many2one('misuse.gs1.log', string='Log', required=True, ondelete='cascade')
     date_time = fields.Datetime(string='Date and Time', required=True, default=lambda self: fields.Datetime.now())
     comments = fields.Text(string='Comments', required=True)
     gs1_agent_email = fields.Char(string='GS1 Agent Email', required=True)
@@ -30,5 +30,4 @@ class MisuseGs1Log(models.Model):
     date_start = fields.Date(string='Start date', required=True)
     date_end = fields.Date(string='End date', required=True)
     remaining_time = fields.Integer(string='Remaining Time (days)')
-    follow_up = fields.One2many('misuse.gs1.log.followup', 'log_id', string='Follow-up')
-
+    follow_up = fields.One2many('misuse.gs1.log.followup', 'misuse_log_followup_id', string='Follow-up')
