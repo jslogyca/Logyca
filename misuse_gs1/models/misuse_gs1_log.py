3f77ecc265bc5832c783a09e07bac2889b6a4dbc
jslogyca <jsocampo@logyca.com>
2025-03-21 15:57:00 -0500

    [IMP] misuse_gs1: UI translation

--- misuse_gs1/models/misuse_gs1_log.py
+++ misuse_gs1/models/misuse_gs1_log.py
@@ -12,14 +12,14 @@ class MisuseGS1LogFollowUp(models.Model):
     _name = 'misuse.gs1.log.followup'
     _description = 'Follow-up for Misuse GS1 Log'
 
-    misuse_log_followup_id = fields.Many2one('misuse.gs1.log', string='Log', required=True, ondelete='cascade')
+    log_id = fields.Many2one('misuse.gs1.log', string='Log', required=True, ondelete='cascade')
     date_time = fields.Datetime(string='Date and Time', required=True, default=lambda self: fields.Datetime.now())
     comments = fields.Text(string='Comments', required=True)
     gs1_agent_email = fields.Char(string='GS1 Agent Email', required=True)
 
 class MisuseGs1Log(models.Model):
     _name = 'misuse.gs1.log'
-    _description = 'Misuse GS1 Log - trazabilidad del mal uso del sistema GS1'
+    _description = 'Misuse GS1 Log'
 
     partner_id = fields.Many2one('res.partner', string='Partner')
     case_id = fields.Char(string='Case ID', required=True)
@@ -30,4 +30,4 @@ class MisuseGs1Log(models.Model):
     date_start = fields.Date(string='Start date', required=True)
     date_end = fields.Date(string='End date', required=True)
     remaining_time = fields.Integer(string='Remaining Time (days)')
-    follow_up = fields.One2many('misuse.gs1.log.followup', 'misuse_log_followup_id', string='Follow-up')
+    follow_up = fields.One2many('misuse.gs1.log.followup', 'log_id', string='Follow-up')
