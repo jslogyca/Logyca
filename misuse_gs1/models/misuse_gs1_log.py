63c2848248a090e51a5c589ef1bbabca1072c489
jslogyca <jsocampo@logyca.com>
2025-03-21 12:54:52 -0500

    [FIX] misuse_gs1: changes try 1

--- misuse_gs1/models/misuse_gs1_log.py
+++ misuse_gs1/models/misuse_gs1_log.py
@@ -7,14 +7,13 @@ class MisuseGs1Log(models.Model):
 
     partner_id = fields.Many2one('res.partner', string='Partner')
     case_id = fields.Char(string='Case ID', required=True)
-    misuse_type = fields.Char(string='Misuse type', description='Misuse type')
-    tracking_type = fields.Char(string='Tracking type', description='Tracking type')
-    state = fields.Char(string='Case status', description='Case status')
+    misuse_type = fields.Char(string='Misuse type')
+    tracking_type = fields.Char(string='Tracking type')
+    state = fields.Char(string='Case status')
     user_attachments = fields.One2many('misuse.gs1.log.attachment', 'log_id', string='User Attachments')
     date_start = fields.Date(string='Start date', required=True)
     date_end = fields.Date(string='End date', required=True)
     remaining_time = fields.Integer(string='Remaining Time (days)')
-    prefixes = fields.Char(string='Prefixes')
 
 class MisuseGs1LogAttachment(models.Model):
     _name = 'misuse.gs1.log.attachment'
