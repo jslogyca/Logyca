f0753072b2d3b49ff822fd2eeeb22cb6da12e037
jslogyca <jsocampo@logyca.com>
2025-04-01 08:45:22 -0500

    [FIX] rvc: web controller issue rendering the view because it was using a deprecated method render_template

--- rvc/controllers/main.py
+++ rvc/controllers/main.py
@@ -5,48 +5,44 @@ from odoo.tools.translate import _
 from odoo.tools.misc import get_lang
 from datetime import datetime
 
+
 class AcceptRvcBenefit(http.Controller):
 
-    @http.route('/rvc/accept_benefit/<string:token>', type='http', auth="public", website=True)
+    @http.route(
+        "/rvc/accept_benefit/<string:token>", type="http", auth="public", website=True
+    )
     def accept_benefit(self, token, **kwargs):
-        postulation_ids = request.env['benefit.application'].sudo().search([('access_token', '=', token)])
+        postulation_ids = (
+            request.env["benefit.application"]
+            .sudo()
+            .search([("access_token", "=", token)])
+        )
         if not postulation_ids:
             return request.not_found()
 
         for postulation_id in postulation_ids:
 
-            # Marcar la posituación como aceptada
-            if postulation_id.state == 'notified':
-                postulation_id.write({'state': 'confirm', 'acceptance_date': datetime.now()})
-                postulation_id.message_post(body=_(\
-                    '%s <u><strong>ACEPTÓ</strong></u> el beneficio desde el correo electrónico.' % str(postulation_id.partner_id.partner_id.name)))
+            if postulation_id.state == "notified":
+                postulation_id.write(
+                    {"state": "confirm", "acceptance_date": datetime.now()}
+                )
+                message = _(
+                    f"{postulation_id.partner_id.partner_id.name} "
+                    "<u><strong>ACEPTÓ</strong></u> el beneficio desde el correo electrónico."
+                )
+                postulation_id.message_post(body=message)
 
-                #agregando adjunto al tercero
+                # agregando adjunto al tercero
                 postulation_id.attach_OM_2_partner(postulation_id)
 
-                if postulation_id.product_id.benefit_type == 'colabora':
+                if postulation_id.product_id.benefit_type == "colabora":
                     postulation_id.calculate_end_date_colabora()
 
-                lang = postulation_id.partner_id.partner_id.lang
-                return request.env['ir.ui.view'].with_context(lang=lang).render_template('rvc.accept_rvc_benefit_page_view', {
-                    'benefit_application': postulation_id, 'token': token
-                })
+                lang = postulation_id.partner_id.partner_id.lang or 'es_ES'
+                request.context = dict(request.context, lang=lang)
 
-    @http.route('/rvc/reject_benefit/<string:token>', type='http', auth="public", website=True)
-    def reject_benefit(self, token, **kwargs):
-        postulation_ids = request.env['benefit.application'].sudo().search([('access_token', '=', token)])
-        if not postulation_ids:
-            return request.not_found()
-
-        for postulation_id in postulation_ids:
-
-            # Marcar la posituación como aceptada
-            if postulation_id.state == 'notified':
-                postulation_id.write({'state': 'rejected', 'rejection_date': datetime.now()})
-                postulation_id.message_post(body=_(\
-                    '%s <u><strong>RECHAZÓ</strong></u> el beneficio desde el correo electrónico.' % str(postulation_id.partner_id.partner_id.name)))
-
-                lang = postulation_id.partner_id.partner_id.lang
-                return request.env['ir.ui.view'].with_context(lang=lang).render_template('rvc.reject_rvc_benefit_page_view', {
-                    'benefit_application': postulation_id, 'token': token
-                })
+                return request.render(
+                    'rvc.accept_rvc_benefit_page_view',
+                    {"benefit_application": postulation_id, "token": token},
+                    True #lazy rendering
+                )
