bc32e8aaa95493108bbf3b25547481f6dc127dfe
Lorena Torres <lorenat0210@gmail.com>
2025-05-06 11:28:25 -0500

    Commit Initial -7

--- account_move_extended/models/__init__.py
+++ account_move_extended/models/__init__.py
@@ -5,7 +5,7 @@ from . import account_move
 # from . import logyca_budget_group
 # from . import purchase_order_line
 from . import account_analytic_account
-# from . import account_analytic_tag
+from . import certification_report
 from . import account_sector_red
 from . import revenue_macro_sector
 from . import res_partner
