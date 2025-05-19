1fcf35b0cc0db25d5c5a6b70ee39f0462970e653
Lorena Torres <lorenat0210@gmail.com>
2025-05-19 09:06:25 -0500

    Active Grupo Presupuestal

--- logyca/models/logyca_budget_group.py
+++ logyca/models/logyca_budget_group.py
@@ -14,6 +14,7 @@ class x_budget_group(models.Model):
     name = fields.Char(string='Nombre', required=True)
     company_id = fields.Many2one('res.company', 'Company', required=True, index=True, default=lambda self: self.env.company.id)
     by_default_group = fields.Boolean('Por Defecto', default=False)
+    active = fields.Boolean(default=True, help="Set active to false to hide the Group without removing it.")
 
     def name_get(self):
         return [(record.id, '%s - %s' %
