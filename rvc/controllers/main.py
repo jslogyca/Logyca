91a9a48bb28cf0e7c34f712f5ecc4aa709720aac
jslogyca <jsocampo@logyca.com>
2025-06-16 17:22:54 -0500

    [ADD] RVC: added code to already accepted benefit controller when a customer use the accept button in the email

--- rvc/controllers/main.py
+++ rvc/controllers/main.py
@@ -9,34 +9,25 @@ class AcceptRvcBenefit(http.Controller):
 
     @http.route('/rvc/accept_benefit/<string:token>', type='http', auth="public", website=True)
     def accept_benefit(self, token, **kwargs):
-        postulation_ids = request.env['benefit.application'].sudo().search([('access_token', '=', token)])
-        if not postulation_ids:
+        postulation_id = request.env['benefit.application'].sudo().search([('access_token', '=', token)], limit=1)
+        if not postulation_id:
             return request.not_found()
 
-        for postulation_id in postulation_ids:
+        postulation_id.ensure_one()
+        lang = postulation_id.partner_id.partner_id.lang or 'es_CO'
 
-            # Marcar la posituación como aceptada
-            if postulation_id.state == 'notified':
-                postulation_id.write({'state': 'confirm', 'acceptance_date': datetime.now()})
-                postulation_id.message_post(body=_(\
-                    '%s <u><strong>ACEPTÓ</strong></u> el beneficio desde el correo electrónico.' % str(postulation_id.partner_id.partner_id.name)))
+        # Execute business logic ONLY if in 'notified' state
+        if postulation_id.state == 'notified':
+            postulation_id.write({'state': 'confirm', 'acceptance_date': datetime.now()})
+            postulation_id.message_post(body=_(\
+                '%s ACEPTÓ el beneficio desde el correo electrónico.' % str(postulation_id.partner_id.partner_id.name)))
 
-                #agregando adjunto al tercero
-                postulation_id.attach_OM_2_partner(postulation_id)
+            # adding attachment to partner
+            postulation_id.attach_OM_2_partner(postulation_id)
 
-                if postulation_id.product_id.benefit_type == 'colabora':
-                    postulation_id.calculate_end_date_colabora()
+            if postulation_id.product_id.benefit_type == 'colabora':
+                postulation_id.calculate_end_date_colabora()
 
-                lang = postulation_id.partner_id.partner_id.lang
-                return request.render('rvc.accept_rvc_benefit_page_view', {
-                    'benefit_application': postulation_id,
-                    'token': token
-                }, headers={'Content-Language': lang})
-
-            # If the benefit was already accepted previously (state 'confirm' or 'done')
-            elif postulation_id.state in ['confirm', 'done']:
-                lang = postulation_id.partner_id.partner_id.lang
-                return request.render('rvc.already_accepted_rvc_benefit_page_view', {
-                    'benefit_application': postulation_id,
-                    'token': token
-                }, headers={'Content-Language': lang})
+        return request.render('rvc.accept_rvc_benefit_page_view', {
+            'benefit_application': postulation_id, 'token': token
+        })
