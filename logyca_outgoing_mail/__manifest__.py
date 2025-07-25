42f66ba211d37ea5b5f5f1fc02d1b32c42c2f13c
jslogyca <jsocampo@logyca.com>
2025-07-25 16:03:29 -0500

    [FIX] logyca_outgoing_mail fixed manifest

--- logyca_outgoing_mail/__manifest__.py
+++ logyca_outgoing_mail/__manifest__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Logyca Outgoing Mail Server Selector',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0',
     'category': 'Mail',
+    'license': 'LGPL-3',
     'summary': 'Automatic outgoing mail server selection based on document type and domain',
     'description': """
         This module automatically selects the appropriate outgoing mail server based on:
