0311da8e09514ef6dfdee695fab18929894ba26c
jslogyca <jsocampo@logyca.com>
2025-08-29 10:39:04 -0500

    [ADD] logyca_mail_bounced_message: New module to customize the bounced email message for mail aliases.

---
+++ logyca_mail_bounced_message/models/mail_alias.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from markupsafe import Markup
+from odoo import models, _
+
+class Alias(models.Model):
+    _inherit = 'mail.alias'
+
+    def _get_alias_invalid_body(self, message_dict):
+        """
+        Genera el cuerpo del correo electrónico de rebote devuelto cuando el alias está
+        configurado incorrectamente (por ejemplo, un error en alias_defaults).
+        """
+        content = Markup(
+            _(
+            "La dirección %(alias_display_name)s no pudo aceptar el mail de abajo. "
+            "Tenga en cuenta que si este es un hilo de correo externo a nuestro sistema "
+            "de tickets, debe enviar un correo aparte para la creación de su caso."
+            )
+        ) % {
+            'alias_display_name': self.display_name,
+        }
+        return self.env['ir.qweb']._render('mail.mail_bounce_alias_security', {
+            'body': Markup('<p>%(header)s,<br /><br />%(content)s<br /><br />%(regards)s</p>') % {
+                'content': content,
+                'header': _('Dear Sender'),
+                'regards': _('Kind Regards'),
+            },
+            'message': message_dict
+        }, minimal_qcontext=True)
