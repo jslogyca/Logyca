513d03544e0a4ac0ee4030bd4fca0ddb5ae6287e
jslogyca <jsocampo@logyca.com>
2025-08-29 10:39:04 -0500

    [ADD] logyca_mail_bounced_message: New module to customize the bounced email message for mail aliases.

---
+++ logyca_mail_bounced_message/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Logyca Mail Bounced Message",
+    'summary': """
+        Modifica el mensaje de correo electrónico rebotado para los alias de correo.""",
+    'description': """
+        Este módulo hereda del módulo 'mail' para personalizar el contenido del correo electrónico que se envía cuando un correo entrante es rechazado por un alias debido a una mala configuración.
+    """,
+    'author': "LOGYCA / SERVICIOS",
+    'website': "https://www.logyca.com",
+    'category': 'Uncategorized',
+    'version': '17.0.1.0.0',
+    'depends': ['mail'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
