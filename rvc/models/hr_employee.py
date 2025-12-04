d861ac45ffbdb1c344ca97a2f7300abac82150cf
jslogyca <jsocampo@logyca.com>
2025-12-04 17:03:03 -0500

    [IMP] rvc: hr_employee x_is_crecemype_consultant improved help

--- rvc/models/hr_employee.py
+++ rvc/models/hr_employee.py
@@ -3,4 +3,8 @@ from odoo import models, fields
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    x_is_crecemype_consultant = fields.Boolean(string="¿Es consultor?", default=False)
+    x_is_crecemype_consultant = fields.Boolean(
+        string="¿Es consultor?",
+        default=False,
+        help="Indica si el empleado es un consultor logístico en LOGYCA / CRECEMYPE."
+    )
